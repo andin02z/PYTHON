26f30d1d6a004056e2b3671748019b3e81220a13
anderson <olva413@gmail.com>
2023-10-18 15:49:37 -0300

    foi adicionado o arquivo input de dados .py

---
+++ aula10.18/input_de_dados.py
@@ -0,0 +1,5 @@
+# para criar uma interação com o usuario ultilizamos a função  input
+# 1. todo input deve ter seu valor associado a uma variavel 
+
+nome = input("digite seu nome aqui: ")
+print(nome)
