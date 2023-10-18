81f79d382161ef3a41ed71fa7a05800fe3b47441
anderson <olva413@gmail.com>
2023-10-18 16:18:07 -0300

    arquivo desafio02.py foi adicionado

---
+++ aula10.18/desafio02.py
@@ -0,0 +1,17 @@
+# criar um arquivo na pasta da aula de hoje 
+# pedir para o usuario  informar a sua idade 
+# pedir para o usuario informar a idade de algum parente sua(mãe, pai, irmão)
+# pedir para o usuario informar o seu nome 
+# imprimir a mutiplicação das duas idades 
+# imprimir o nome do usuario 
+# obs: somente a idade de parente  
+# obs: faça todos os commit e updates do GIT/GITHUB
+# obs: para mudar para string faça int (nome)
+
+idade = input ("digite sua idade aqui: ")
+
+idade_pt = input ("idade  do seu irmao: ")
+nome = input ("informe seu nome: ")
+print (int(idade) *int(idade_pt))
+print(nome)
+
