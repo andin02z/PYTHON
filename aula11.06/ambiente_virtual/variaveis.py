6e86f547ce3f5d94cd1a752ca6e63a4c58df2cbd
anderson <olva413@gmail.com>
2023-11-06 16:34:55 -0300

    variaveis.py foi adicionada aula11.06

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,12 @@
+# variaveis são usadas para salvar algo na memoria do computador.
+# o sina de "=" é ultilizado para atribução de valores
+var01 = "alguma coisa"
+print (var01)
+
+var01 = "outra coisa"
+print(var01)
+
+var02 = "A" + "B" + "C"
+print(var02) 
+var02 = "A" * 10
+print(var02)
