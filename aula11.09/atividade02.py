d36e2b7405eb7129645540afee2d208639e2600d
anderson <olva413@gmail.com>
2023-11-09 16:08:30 -0300

    atividades do dia11.09

---
+++ aula11.09/atividade02.py
@@ -0,0 +1,12 @@
+
+# faça fatiamento de 8 palavras e forme uma frase com a impressão dessas palavras
+texto = "somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. ambiente que propociiona conexã, de troca de conhecimentos e aprendizado compartilhado entre participantes, intrutores e empresas parecidas"
+
+print(texto[0:5])
+print(texto[8:9])
+print(texto[10:16])
+print(texto[17:19])
+print(texto[20:31])
+print(texto[45:48])
+print(texto[49:54])
+print(texto[76:89])
