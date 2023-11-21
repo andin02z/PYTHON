d3b330fe03f7c91471116f494bddb5e9c08a179c
anderson <olva413@gmail.com>
2023-11-21 19:04:13 -0300

    atv do dia 21/11

---
+++ aula11.21/atv.py
@@ -0,0 +1,10 @@
+lista_semana =['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado']
+print(lista_semana)
+
+lista_enumerada = enumerate (lista_semana)
+
+print(lista_semana)
+print(lista_enumerada)
+
+for lista in lista_enumerada:
+    print(lista)
---
+++ aula11.21/for.py
@@ -0,0 +1,16 @@
+# inter ()
+# next ()
+
+listas_nomes  = [ 'joaõ', 'pedro', 'mateus', 'judas',' tiago'] 
+print(listas_nomes)
+lista_enumerada = enumerate(listas_nomes)
+
+
+print(listas_nomes)
+print(lista_enumerada)
+
+for item in lista_enumerada:
+    print(item)
+
+for item_lista in listas_nomes:
+    print(listas_nomes)
