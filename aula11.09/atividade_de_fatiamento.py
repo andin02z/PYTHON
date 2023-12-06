22da58cfcd0b2cc21704aa20e2003ad127fb1f7e
anderson <olva413@gmail.com>
2023-12-06 13:53:14 -0300

    nem sei

--- aula11.09/atividade_de_fatiamento.py
+++ aula11.09/atividade_de_fatiamento.py
@@ -4,14 +4,4 @@
 texto = "somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. ambiente que propociiona conexã, de troca de conhecimentos e aprendizado compartilhado entre participantes, intrutores e empresas parecidas"
 
 print(texto [0:5])
-print(texto[6:9])
-print(texto[10:16])
-print(texto[17:19])
-print(texto[20:31])
-print(texto[31:34])
-print(texto[34:44])
-print(texto[45:48])
-print(texto[49:54])
-print(texto[54:61])
-
 
