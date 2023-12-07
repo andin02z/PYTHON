4c4597ed284f76286f12856a5a9024dc0ec3e748
anderson <olva413@gmail.com>
2023-12-07 16:39:28 -0300

    atividade para corrigir

--- aula12.06/atv05.py
+++ aula12.06/atv05.py
@@ -1,1 +1,4 @@
 #crie um sistema de login e senha. crie um dicionario contendo os acessos dos colaboradores com nome de usuario e senha. em seguida peça as informações e valide o login do usuario
+# faça um quiz ultilizando um dicionario com as seguintes chaves: pergunta, opções e resposta. faça a validação da opção escolhida com a resposta e imprima
+dic_perguntas = {'pergunta': 'alguma coisa''opções' }
+print(dic_perguntas)
---
+++ aula12.07.py/atv01.py
@@ -0,0 +1,25 @@
+# faça um quiz ultilizando um dicionario com as seguintes chaves: pergunta, opções e resposta. faça a validação com a resposta e imprima
+perguntas = {'pergunta':'quanto é 5 x 5?',
+             'opçoes':[12, 16, 20, 25],
+             'resposta':25,},
+for pergunta in perguntas:
+    print('pergunta:',pergunta['pergunta'])
+    print()
+
+    print(pergunta['opçoes'])
+    for i, opçao in enumerate(pergunta['opçao'])
+    print(f'{i+1}')
+
+
+    escolha = int(input(escolha sua opção:))
+    acertou = False
+
+    if escolha == int(pergunta['resposta'])
+    acertou = True
+    print()
+    if acertou :
+    else:
+        print('errou')
+
+        print()
+        
